7132d18aed227b48f922209a9d5eda04155f850f
aryabawanta <arya.bawanta@gmail.com>
2020-01-17 23:38:16 +0800

    add route

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    for ($i = 0; $i < 50; $i++) {
+        \App\Jobs\FindFavoriteOS::dispatch();
+    }
+
+    return '50 Jobs dispatched!';
 });
